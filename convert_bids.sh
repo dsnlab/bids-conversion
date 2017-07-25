bfd96b6628b4561925deb6d6f60f8997f41f9da1
Dani Cosme <dani.cosme@gmail.com>
2017-07-24 17:10:35 -0700

    added logic to assess whether files are missing and output errors accordingly

--- convert_bids.sh
+++ convert_bids.sh
@@ -46,8 +46,8 @@ if [ "${convertanat}" == "TRUE" ]; then
 	echo -e "\nConverting anatomical mprage into nifti"
 	mkdir anat
 	anatomicaloutput="$niidir/${subid}/anat"
-	mcverter -o "$anatomicaloutput"/ --format=nifti --nii --match="${anat}" -F -PatientName-PatientId-SeriesDate-SeriesTime-StudyId-StudyDescription+SeriesNumber-SequenceName-ProtocolName+SeriesDescription $dicomdir
-	cd "$anatomicaloutput"
+	mcverter -o "${anatomicaloutput}"/ --format=nifti --nii --match="${anat}" -F -PatientName-PatientId-SeriesDate-SeriesTime-StudyId-StudyDescription+SeriesNumber-SequenceName-ProtocolName+SeriesDescription $dicomdir
+	cd "${anatomicaloutput}"
 	du -sh *.nii
 	gzip -f *.nii
 fi
@@ -57,8 +57,8 @@ if [ "${convertfmap}" == "TRUE" ]; then
 	cd "$niidir"/"${subid}"
 	mkdir fmap
 	fmapoutput="$niidir/${subid}/fmap"
-	mcverter -o "$fmapoutput"/ --format=nifti --nii --match="${fmap}" -F -PatientName-PatientId-SeriesDate-SeriesTime-StudyId-StudyDescription+SeriesNumber-SequenceName-ProtocolName+SeriesDescription $dicomdir
-	cd "$fmapoutput"
+	mcverter -o "${fmapoutput}"/ --format=nifti --nii --match="${fmap}" -F -PatientName-PatientId-SeriesDate-SeriesTime-StudyId-StudyDescription+SeriesNumber-SequenceName-ProtocolName+SeriesDescription $dicomdir
+	cd "${fmapoutput}"
 	du -sh *.nii
 	gzip -f *.nii
 fi
@@ -68,8 +68,8 @@ if [ "${convertdti}" == "TRUE" ]; then
 	cd "$niidir"/"${subid}"
 	mkdir dti
 	dtioutput="$niidir/${subid}/dti"
-	mcverter -o "$dtioutput"/ --format=nifti --nii --fourd --match="${dti}" -F -PatientName-PatientId-SeriesDate-SeriesTime-StudyId-StudyDescription+SeriesNumber-SequenceName-ProtocolName+SeriesDescription $dicomdir
-	cd "$dtioutput"
+	mcverter -o "${dtioutput}"/ --format=nifti --nii --fourd --match="${dti}" -F -PatientName-PatientId-SeriesDate-SeriesTime-StudyId-StudyDescription+SeriesNumber-SequenceName-ProtocolName+SeriesDescription $dicomdir
+	cd "${dtioutput}"
 	du -sh *.nii
 	gzip -f *.nii
 fi
@@ -80,9 +80,9 @@ if [ "${convertrest}" == "TRUE" ]; then
 	mkdir resting
 	restingoutput="$niidir/${subid}/resting"
 	for rest in ${resting[@]}; do 
-		mcverter -o "$restingoutput"/ --format=nifti --nii --fourd --match="${rest}" -F -PatientName-PatientId-SeriesDate-SeriesTime-StudyId-StudyDescription+SeriesNumber-SequenceName-ProtocolName+SeriesDescription $dicomdir
+		mcverter -o "${restingkoutput}"/ --format=nifti --nii --fourd --match="${rest}" -F -PatientName-PatientId-SeriesDate-SeriesTime-StudyId-StudyDescription+SeriesNumber-SequenceName-ProtocolName+SeriesDescription $dicomdir
 	done
-	cd "$restingoutput"
+	cd "${restingkoutput}"
 	du -sh *.nii
 	gzip -f *.nii
 fi
@@ -93,9 +93,9 @@ if [ "${converttask}" == "TRUE" ]; then
 	mkdir task
 	taskoutput="$niidir/${subid}/task"
 	for task in ${tasks[@]}; do
-		mcverter -o "$taskoutput"/ --format=nifti --nii --fourd --match=${task} -F -PatientName-PatientId-SeriesDate-SeriesTime-StudyId-StudyDescription+SeriesNumber-SequenceName-ProtocolName+SeriesDescription $dicomdir
+		mcverter -o "${taskoutput}"/ --format=nifti --nii --fourd --match=${task} -F -PatientName-PatientId-SeriesDate-SeriesTime-StudyId-StudyDescription+SeriesNumber-SequenceName-ProtocolName+SeriesDescription $dicomdir
 	done
-	cd "$taskoutput"
+	cd "${taskoutput}"
 	du -sh *.nii
 	gzip -f *.nii
 fi
@@ -113,14 +113,18 @@ if [ "${converttask}" == "TRUE" ]; then mkdir -v func; fi
 # structural (mprage)
 if [ "${convertanat}" == "TRUE" ]; then
 	echo -e "\nCopying structural"
-	if [ $(ls "$anatomicaloutput"/*"${anat}".nii.gz | wc -l) -eq 1 ]; then
-		cp ${cpflags} "$anatomicaloutput"/*"${anat}".nii.gz "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/anat/sub-"${subid}"_ses-"${sessid}"_T1w.nii.gz
+	if [ $(ls "${anatomicaloutput}"/*"${anat}".nii.gz | wc -l) -eq 1 ]; then
+		cp ${cpflags} "${anatomicaloutput}"/*"${anat}".nii.gz "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/anat/sub-"${subid}"_ses-"${sessid}"_T1w.nii.gz
+	elif [ $(ls "${anatomicaloutput}"/*"${anat}".nii.gz | wc -l) -eq 0 ]; then
+		# print missing file paths in errorlog.txt if = 0 files
+		echo "ERROR: no files; nothing to copy"
+		echo "${anatomicaloutput}": MISSING "${anat}" >> $errorlog
 	else 
 		# print file paths in errorlog.txt if =~ 1 file; copy both files
 		echo "ERROR: wrong number of files; all files copied"
-		ls "$anatomicaloutput"/*"${anat}".nii.gz >> $errorlog
-		t1w1=$(ls "$anatomicaloutput"/*"${anat}".nii.gz | head -1)
-		t1w2=$(ls "$anatomicaloutput"/*"${anat}".nii.gz | tail -1)
+		ls "${anatomicaloutput}"/*"${anat}".nii.gz >> $errorlog
+		t1w1=$(ls "${anatomicaloutput}"/*"${anat}".nii.gz | head -1)
+		t1w2=$(ls "${anatomicaloutput}"/*"${anat}".nii.gz | tail -1)
 		cp ${cpflags} "${t1w1}" "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/anat/sub-"${subid}"_ses-"${sessid}"_run-01_T1w.nii.gz
 		cp ${cpflags} "${t1w2}" "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/anat/sub-"${subid}"_ses-"${sessid}"_run-02_T1w.nii.gz
 	fi
@@ -129,29 +133,33 @@ fi
 # fieldmaps
 if [ "${convertfmap}" == "TRUE" ]; then
 	echo -e "\nCopying fieldmaps"
-	if [ "${fieldmapEPI}" == "TRUE" ]; then
-		ap=$(ls -f "$fmapoutput"/*_ap.nii.gz | head -1)
-		pa=$(ls -f "$fmapoutput"/*_pa.nii.gz | head -1)
+	if [ $(ls "${fmapoutput}"/*"${fmap}"*.gz | wc -l) -eq 0 ]; then
+		# print missing file paths in errorlog.txt if = 0 files
+		echo "ERROR: no files; nothing to copy"
+		echo "${fmapoutput}": MISSING "${fmap}" >> $errorlog
+	elif [ "${fieldmapEPI}" == "TRUE" ]; then
+		ap=$(ls -f "${fmapoutput}"/*_ap.nii.gz | head -1)
+		pa=$(ls -f "${fmapoutput}"/*_pa.nii.gz | head -1)
 		cp ${cpflags} "${ap}"  "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/fmap/sub-"${subid}"_ses-"${sessid}"_dir-ap_epi.nii.gz
 		cp ${cpflags} "${pa}"  "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/fmap/sub-"${subid}"_ses-"${sessid}"_dir-pa_epi.nii.gz
 		
-		# print file paths in errorlog.txt if > 2 files
-		if [ $(ls "$fmapoutput"/*_ap.nii.gz | wc -l) -gt 2 ]; then
+		# print file paths in errorlog.txt if =~ 2 files
+		if [ $(ls "${fmapoutput}"/*_ap.nii.gz | wc -l) -ne 2 ]; then
 			echo "ERROR: wrong number of files"
-			ls "$fmapoutput"/*_ap.nii.gz >> $errorlog
+			ls "${fmapoutput}"/*_ap.nii.gz >> $errorlog
 		fi
 	else
-		phase=$(ls -f "$fmapoutput"/*"${fmap}".nii.gz)
-		mag1=$(ls -f "$fmapoutput"/*"${fmap}"_01.nii.gz)
-		mag2=$(ls -f "$fmapoutput"/*"${fmap}"_02.nii.gz)
+		phase=$(ls -f "${fmapoutput}"/*"${fmap}".nii.gz)
+		mag1=$(ls -f "${fmapoutput}"/*"${fmap}"_01.nii.gz)
+		mag2=$(ls -f "${fmapoutput}"/*"${fmap}"_02.nii.gz)
 		cp ${cpflags} "${phase}"  "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/fmap/sub-"${subid}"_ses-"${sessid}"_phasediff.nii.gz
 		cp ${cpflags} "${mag1}"  "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/fmap/sub-"${subid}"_ses-"${sessid}"_magnitude1.nii.gz
 		cp ${cpflags} "${mag2}"  "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/fmap/sub-"${subid}"_ses-"${sessid}"_magnitude2.nii.gz
 
-		# print file paths in errorlog.txt if > 3 files
-		if [ $(ls "$fmapoutput"/*"${fmap}"*.gz | wc -l) -gt 3 ]; then
+		# print file paths in errorlog.txt if =~ 3 files
+		if [ $(ls "${fmapoutput}"/*"${fmap}"*.gz | wc -l) -ne 3 ]; then
 			echo "ERROR: wrong number of files"
-			ls "$fmapoutput"/*"${fmap}"*.gz >> $errorlog
+			ls "${fmapoutput}"/*"${fmap}"*.gz >> $errorlog
 		fi
 	fi
 fi
@@ -159,13 +167,17 @@ fi
 # DTI 
 if [ "${convertdti}" == "TRUE" ]; then
 	echo -e "\nCopying DTI"
-	if [ $(ls "$dtioutput"/*"${dti}"*.nii.gz | wc -l) -eq 2 ]; then
-		cp ${cpflags} "$dtioutput"/*"${dti}"*rl.nii.gz "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/dwi/sub-"${subid}"_ses-"${sessid}"_dir-rl_dwi.nii.gz
-		cp ${cpflags} "$dtioutput"/*"${dti}"*lr.nii.gz "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/dwi/sub-"${subid}"_ses-"${sessid}"_dir-lr_dwi.nii.gz
+	if [ $(ls "${dtioutput}"/*"${dti}"*.nii.gz | wc -l) -eq 2 ]; then
+		cp ${cpflags} "${dtioutput}"/*"${dti}"*rl.nii.gz "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/dwi/sub-"${subid}"_ses-"${sessid}"_dir-rl_dwi.nii.gz
+		cp ${cpflags} "${dtioutput}"/*"${dti}"*lr.nii.gz "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/dwi/sub-"${subid}"_ses-"${sessid}"_dir-lr_dwi.nii.gz
+	elif [ $(ls "${dtioutput}"/*"${dti}"*.nii.gz | wc -l) -eq 0 ]; then
+		# print missing file paths in errorlog.txt if = 0 files
+		echo "ERROR: no files; nothing to copy"
+		echo "${dtioutput}": MISSING "${dti}" >> $errorlog
 	else
 		# print file paths in errorlog.txt if =~ 2 files; do not copy file
 		echo "ERROR: wrong number of files; files not copied"
-		ls "$dtioutput"/*"${dti}"*.nii.gz >> $errorlog
+		ls "${dtioutput}"/*"${dti}"*.nii.gz >> $errorlog
 	fi
 fi
 
@@ -173,13 +185,17 @@ fi
 if [ "${convertrest}" == "TRUE" ]; then
 	echo -e "\nCopying resting state"
 	for rest in ${resting[@]}; do 
-		if [ $(ls "$restingoutput"/*"${rest}".nii.gz | wc -l) -eq 1 ]; then
+		if [ $(ls "${restingoutput}"/*"${rest}".nii.gz | wc -l) -eq 1 ]; then
 			runnum="$(echo "${rest}" | tail -c 2)"
-			cp ${cpflags} "$restingoutput"/*"${rest}".nii.gz  "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/func/sub-"${subid}"_ses-"${sessid}"_task-rest_run-0"${runnum}"_bold.nii.gz
+			cp ${cpflags} "${restingkoutput}"/*"${rest}".nii.gz  "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/func/sub-"${subid}"_ses-"${sessid}"_task-rest_run-0"${runnum}"_bold.nii.gz
+		elif [ $(ls "${restingoutput}"/*"${rest}".nii.gz  | wc -l) -eq 0 ]; then
+			# print missing file paths in errorlog.txt if = 0 files
+			echo "ERROR: no files; nothing to copy"
+			echo "${restingoutput}": MISSING "${rest}" >> $errorlog
 		else
 			# print file paths in errorlog.txt if =~ 1 files; do not copy file
 			echo "ERROR: wrong number of files; files not copied"
-			ls "$restingoutput"/*"${rest}".nii.gz >> $errorlog
+			ls "${restingoutput}"/*"${rest}".nii.gz >> $errorlog
 		fi
 	done
 fi
@@ -190,17 +206,21 @@ if [ "${converttask}" == "TRUE" ]; then
 	for task in ${tasks[@]}; do 
 		runnum="$(echo "${task}" | sed 's/[^0-9]//g')"
 		taskalpha="$(echo "${task}" | sed 's/[^a-zA-Z]//g')"
-		if [ $(ls "$taskoutput"/*"${task}"*.nii.gz | wc -l) -eq 1 ]; then
+		if [ $(ls "${taskoutput}"/*"${task}"*.nii.gz | wc -l) -eq 1 ]; then
 			if [[ $runnum =~ ^[0-9]+$ ]]; then 
-				cp ${cpflags} "$taskoutput"/*"${task}"*.nii.gz "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/func/sub-"${subid}"_ses-"${sessid}"_task-"${taskalpha}"_run-0"${runnum}"_bold.nii.gz
+				cp ${cpflags} "${taskoutput}"/*"${task}"*.nii.gz "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/func/sub-"${subid}"_ses-"${sessid}"_task-"${taskalpha}"_run-0"${runnum}"_bold.nii.gz
 			else
-				cp ${cpflags} "$taskoutput"/*"${task}"*.nii.gz "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/func/sub-"${subid}"_ses-"${sessid}"_task-"${taskalpha}"_run-01_bold.nii.gz
+				cp ${cpflags} "${taskoutput}"/*"${task}"*.nii.gz "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/func/sub-"${subid}"_ses-"${sessid}"_task-"${taskalpha}"_run-01_bold.nii.gz
 			fi
+		elif [ $(ls "${taskoutput}"/*"${task}"*.nii.gz | wc -l) -eq 0 ]; then
+			# print missing file paths in errorlog.txt if = 0 files
+			echo "ERROR: no files; nothing to copy"
+			echo "${taskoutput}": MISSING "${task}" >> $errorlog
 		else
 			# print file paths in errorlog.txt if =~ 1 files; copy the largest file
 			echo "ERROR: wrong number of files; largest file copied"
-			ls "$taskoutput"/*"${task}"*.nii.gz >> $errorlog
-			largestfile=$(du -sh "$taskoutput"/*"${task}"*.nii.gz | sort -n | tail -1 | cut -f2)
+			ls "${taskoutput}"/*"${task}"*.nii.gz >> $errorlog
+			largestfile=$(du -sh "${taskoutput}"/*"${task}"*.nii.gz | sort -n | tail -1 | cut -f2)
 			if [[ $runnum =~ ^[0-9]+$ ]]; then 
 				cp ${cpflags} "${largestfile}" "$bidsdir"/sub-"${subid}"/ses-"${sessid}"/func/sub-"${subid}"_ses-"${sessid}"_task-"${taskalpha}"_run-0"${runnum}"_bold.nii.gz
 			else
